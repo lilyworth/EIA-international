b38d40e8bc74f4dfa045057b3aa91f0a159ab34e
Lily <lilyworth@gmail.com>
2016-02-18 19:37:11 +0000

    Added content to page template

--- wp-content/themes/eia-v2/page-inspired-campaign.php
+++ wp-content/themes/eia-v2/page-inspired-campaign.php
@@ -6,6 +6,201 @@ Template name: Inspired campaign
  */
  get_header(inspired); ?>
 
-<p>This is just a test, please ignore</p>
+<div class="lwpagewrapper">
+    <article class="lwhero">
+        <div class="lwheroinner">
+            <div id="lwmonthmod" class="lwrow">
+                <div class="lwmodule">
+                    <div class="lwmoduleinner">
+                         <div class="lwmoduleheader">
+                            <h2>Give £5 a month and help investigators undercover</h2>
+                         </div>
+                        <img src="images/skins.jpg" alt="skins">
+                        <p>£5 a month could help pay for digital voice recorder pens used to record meetings with wildlife criminals and corrupt officials.</p>
+                    </div>
+                </div>
+                <div class="lwmodule lw_mob_hide">
+                    <div class="lwmoduleinner">
+                         <div class="lwmoduleheader">
+                            <h2>Give £10 a month and help gather valuable evidence</h2>
+                         </div>
+                        <img src="images/skins.jpg" alt="skins">
+                        <p>£10 a month could help pay for photographic equipment such as memory cards that an investigator would need to document criminal activities.</p>
+                    </div>
+                </div>
+                <div class="lwmodule lw_mob_hide">
+                    <div class="lwmoduleinner">
+                         <div class="lwmoduleheader">
+                            <h2>Give £15 a month and help investigators on the front line</h2>
+                         </div>
+                         <img src="images/skins.jpg" alt="skins">
+                        <p>£15 a month could help pay for covert GPS systems that can help track illegal timber flows in real time.</p>
+                    </div>
+                </div>
+            </div>
+            <div id="lwsinglemod" class="lwrow lwhide">
+                <div class="lwmodule">
+                    <div class="lwmoduleinner">
+                         <div class="lwmoduleheader">
+                            <h2>£10 could pay for a memory card</h2>
+                         </div>
+                        <img src="images/gps.jpg" alt="GPS">
+                        <p>Investigators need photographic equipment to record illegal activity and present evidence of animal trading to international agencies.</p>
+                    </div>
+                </div>
+                <div class="lwmodule lw_mob_hide">
+                    <div class="lwmoduleinner">
+                         <div class="lwmoduleheader">
+                            <h2>£25 could buy a USB drive</h2>
+                         </div>
+                        <img src="images/gps.jpg" alt="GPS">
+                        <p>These are used to safely store and transport EIA reports and films that we use to lobby decision makers.</p>
+                    </div>
+                </div>
+                <div class="lwmodule lw_mob_hide">
+                    <div class="lwmoduleinner">
+                         <div class="lwmoduleheader">
+                            <h2>£50 could pay for a pair of&nbsp;binoculars</h2>
+                         </div>
+                         <img src="images/gps.jpg" alt="GPS">
+                        <p>Often our investigators need to observe illegal activity such as logging from a safe distance, but still need to be able to recognise key perpetrators.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="lwrow lwpadt20">
+                <form>
+                    <div class="lwfreq">
+                        <input type="radio" id="r1" name="frequency" checked="checked"><label id="lwr1">Monthly</label>
+                        <span class="lwrb_fullwidth"><input type="radio" id="r2" name="frequency"><label>Single</label></span>
+                    </div>
+                    <div id="lwmonthly">
+                        <div id="lw_amount1" class="lwamount">£3</div>
+                        <div id="lw_amount2" class="lwamount lwactive">£5</div>
+                        <div id="lw_amount3" class="lwamount">£10</div>
+                        <div id="lw_amount4" class="lwamount">£15</div>
+                    </div>
+                    <div id="lwsingle" class="lwhide">
+                        <div id="lw_amount5" class="lwamount lwactive">£10</div>
+                        <div id="lw_amount6" class="lwamount">£25</div>
+                        <div id="lw_amount7" class="lwamount">£50</div>
+                        <div id="lw_amount8" class="lwamount">£100</div>
+                    </div>
+                    <div class="lwother">
+                        <div class="lwtxtfield">
+                            <div class="lwpound">£</div>
+                            <input type="text" placeholder="Other">
+                        </div>
+                        <input type="submit" value="Donate" class="lwbtn">
+                    </div>
+                </form>
+            </div>
+        </div>
+    </article>
+    <article class="lwmain">
+        <div class="lwrow lwpadtb40">
+            <div class="lw_intro_content">
+                <h1>Join EIA in the fight against wildlife crime</h1>
+                <p class="lwstrong">Join us as we expose the criminals who threaten our planet.</p>
+                <p>From deforestation and climate change to protecting endangered species and oceans, we’re committed to investigating and exposing any activity that harms our planet. But we need your help.</p>
+                <p>Our investigators work in very challenging conditions to expose illegal practices – often providing valuable information to Interpol and customs agencies worldwide. But we rely on the generous support of the public. It costs between £10,000 and £20,000 to run a single investigation and, without the generosity of people who care about protecting our planet, criminal networks will continue to operate with impunity despite the damage they cause. So please, help us fight environmental and wildlife crime with a gift today.</p>
+            </div>
+            <div class="lw_subnav">
+                <h3>On this page</h3>
+                <ul>
+                    <li><a href="#al1">Investigations</a></li>
+                    <li><a href="#al2">Campaigning</a></li>
+                    <li><a href="#sos">Punching above our weight</a></li>
+                    <li><a href="#al4">Cooperation</a></li>
+                    <li><a href="#al5">Global reach</a></li>
+                </ul>
+            </div>
+        </div>
+    </article>
+    <article class="lw_two-tiles_dark1">
+        <a id="al1"></a>
+        <div class="lw_two-tiles_dark2" id="lw_investigations_img">
+            <div class="lw_two-tiles_tile"><span class="lw_mob_hide">&nbsp;</span><img class="lw_mob_show" src="images/gps.jpg" alt="gps"></div>
+            <div class="lw_two-tiles_tile">
+                <div class="lw_two-tiles_inner_l">
+                    <h2>Investigations</h2>
+                    <p>Diligent, carefully planned undercover investigations are at the heart of EIA’s work, taking us out to the front lines of environmental crime and returning with the credible intelligence and persuasive imagery needed to confirm the findings of our preliminary research and tip-offs from the field.</p>
+                    <p>The more complex cases can see investigators setting up false front companies and produce well-researched fake identities, allowing them to infiltrate criminal organisations and get close to the key individuals involved.</p>
+                    <button class="lwbtn">Help us investigate wildlife&nbsp;crime</button>
+                </div>
+            </div>
+        </div>
+    </article>
+    <article class="lw_two-tiles_light1">
+        <a id="al2"></a>
+        <div class="lw_two-tiles_light2" id="lw_campaigning_img">
+            <div class="lw_mobile_show lw_two-tiles_tile"><img class="lw_mob_show" src="images/gps.jpg" alt=""></div>
+            <div class="lw_two-tiles_tile2">
+                <div class="lw_two-tiles_inner_r">
+                    <h2>Campaigning</h2>
+                    <p>The evidence sourced in the field is used by EIA to raise awareness and to advocate for meaningful change and policy reforms, lobbying those in power and producing authoritative reports and compelling short films.</p>
+                    <p>We also identify and advocate solutions to the problems we expose, a major focus of which is the role played by consumer demand for products harming the environment, from illegal logging to supermarket refrigeration.</p>
+                    <button class="lwbtn">Help us expose illegal&nbsp;threats</button>
+                </div>
+            </div>
+            <div class="lw_mobile_hide lw_two-tiles_tile"><span class="lw_mob_hide">&nbsp;</span></div>
+        </div>
+        
+    </article>   
+    <article class="lwmain">
+        <div class="lwrow lwpadtb40">
+            <a id="sos"></a>
+            <div class="lwgrid6">
+                <h1>Punching above our weight</h1>
+                <p class="lwstrong">Join us as we expose the criminals who threaten our planet.</p>
+                <p>EIA is a small organisation making a big difference. We don’t have flashy offices or huge salaries but instead, we give great value for money by focusing our resources on the vital work which has made us one of the most effective conservation organisations in the world.</p>
+                <p>To keep making a difference we rely on the generosity and vision of our supporters, who understand the importance of protecting our wildlife and our environment.</p>
+            </div>
+            <div class="lwgrid6">
+                <h3>For every £1 given to EIA we spend:</h3>
+                <div class="lwspend_pie"><img src="images/pie-chart.png" alt="pic chart showing spend"></div>
+                <div class="lwspend_data">
+                    <div class="lwspend_data_module_1">
+                        <p>72p on investigating and campaigning for environmental protection</p>
+                    </div>
+                    <div class="lwspend_data_module_2">
+                        <p>20p on overheads and running costs</p>
+                    </div>
+                    <div class="lwspend_data_module_3">
+                        <p>8p on fundraising and communications</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </article>
+    <article class="lw_two-tiles_dark1">
+        <a id="al4"></a>
+        <div class="lw_two-tiles_dark2" id="lw_cooperation_img">
+            <div class="lw_two-tiles_tile"><span class="lw_mob_hide">&nbsp;</span><img class="lw_mob_show" src="images/gps.jpg" alt=""></div>
+            <div class="lw_two-tiles_tile">
+                <div class="lw_two-tiles_inner_l">
+                    <h2>Cooperation</h2>
+                    <p>EIA works closely with a number of other organisations, developing close partnerships with NGOs around the world as well as engaging with wildlife and customs enforcement agencies at national and international levels.</p>
+                    <p>For example, we have provided hard intelligence to Interpol and the World Customs Organisation, while our bespoke training films on illegal trades are used by enforcement agencies worldwide. We also work with local communities, providing them with valuable training and empowering them to protect their environment.</p>
+                    <button class="lwbtn">Help us empower local&nbsp;communities</button>
+                </div>
+            </div>
+        </div>
+    </article>
+    <article class="lw_two-tiles_light1">
+        <a id="al5"></a>
+        <div class="lw_two-tiles_light2" id="lw_global_img">
+            <div class="lw_mobile_show lw_two-tiles_tile"><img class="lw_mob_show" src="images/gps.jpg" alt=""></div>
+            <div class="lw_two-tiles_tile2">
+                <div class="lw_two-tiles_inner_r">
+                    <h2>Global reach</h2>
+                    <p>From uncovering criminal syndicates behind the decimation of elephant populations in Tanzania to exposing the illegal trade of animal parts and products in China, our commitment to protecting wildlife and the environment has no borders.</p>
+                    <p>When we discover corruption, greed and exploitation that puts our natural world in jeopardy, we do everything we can to expose and report it to the relevant authorities.</p>
+                    <button class="lwbtn">Send a gift today</button>
+                </div>
+            </div>
+            <div class="lw_mobile_hide lw_two-tiles_tile"><span class="lw_mob_hide">&nbsp;</span></div>
+        </div>
+    </article>
+</div>
 
 <?php get_footer(); ?>
